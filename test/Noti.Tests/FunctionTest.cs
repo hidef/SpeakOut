0cdcc09733dee50f873e5f93fb9afc268961f3f5
Robert Stiff <uatecuk@gmail.com>
2017-05-05 07:58:59 +0100

    Remove templated test

--- test/Noti.Tests/FunctionTest.cs
+++ test/Noti.Tests/FunctionTest.cs
@@ -21,18 +21,6 @@ namespace Noti.Tests
         [Fact]
         public void TetGetMethod()
         {
-            TestLambdaContext context;
-            APIGatewayProxyRequest request;
-            APIGatewayProxyResponse response;
-
-            Functions functions = new Functions();
-
-
-            request = new APIGatewayProxyRequest();
-            context = new TestLambdaContext();
-            response = functions.Get(request, context);
-            Assert.Equal(200, response.StatusCode);
-            Assert.Equal("Hello AWS Serverless", response.Body);
         }
     }
 }
