2d0184037c0538e3407a32b3743b54fa7b899ebc
Robert Stiff <uatecuk@gmail.com>
2017-05-06 21:33:03 +0100

    Current redis hosting only has two DBs, share codes and addressbooks, they will probably not clash.

--- src/Noti/Intents/RedisDBs.cs
+++ src/Noti/Intents/RedisDBs.cs
@@ -4,6 +4,6 @@ namespace Noti.Intents
     {
         public static int MailBoxes = 0;
         public static int Codes = 1;
-        public static int AddressBooks = 2;
+        public static int AddressBooks = 1;
     }
 }
