524dccec137e71b6f4843d1ebe4b597c5cc682e3
Robert Stiff <uatecuk@gmail.com>
2017-05-09 00:37:06 +0100

    All e2e tests.

--- test/Noti.Tests/FunctionTest.cs
+++ test/Noti.Tests/FunctionTest.cs
@@ -12,6 +12,7 @@ using Noti;
 using NSubstitute;
 using Slight.Alexa.Framework.Models.Requests;
 using Slight.Alexa.Framework.Models.Requests.RequestTypes;
+using Slight.Alexa.Framework.Models.Responses;
 
 namespace Noti.Tests
 {
@@ -22,7 +23,67 @@ namespace Noti.Tests
         }
 
         [Fact]
-        public void TetGetMethod()
+        public void GetCode()
+        {
+            Functions f = new Functions();
+            ILambdaContext ctx = Substitute.For<ILambdaContext>();
+            var request = new SkillRequest
+            {
+                Request = new RequestBundle
+                {
+                    Type = "IntentRequest",
+                    Intent = new Intent
+                    {
+                        Name = "GetCode",
+                        Slots = new Dictionary<string, Slot> {
+                        }
+                    }
+                },
+                Session = new Session {
+                    User = new User {
+                        UserId = "jill"
+                    }
+                }
+            };
+            var response = f.FunctionHandler(request, null);
+
+            Console.WriteLine(((PlainTextOutputSpeech) response.Response.OutputSpeech).Text);
+        }
+        
+        [Fact]
+        public void Befriend()
+        {
+            Functions f = new Functions();
+            ILambdaContext ctx = Substitute.For<ILambdaContext>();
+            var request = new SkillRequest
+            {
+                Request = new RequestBundle
+                {
+                    Type = "IntentRequest",
+                    Intent = new Intent
+                    {
+                        Name = "Befriend",
+                        Slots = new Dictionary<string, Slot> {
+                            {"name", new Slot { Value = "jack"}},
+                            {"code", new Slot { Value = "499"}}
+                        }
+                    }
+                },
+                Session = new Session {
+                    User = new User {
+                        UserId = "jill"
+                    }
+                }
+            };
+            var response = f.FunctionHandler(request, null);
+
+
+            Console.WriteLine(((PlainTextOutputSpeech) response.Response.OutputSpeech).Text);
+
+        }
+        
+        [Fact]
+        public void Tell()
         {
             Functions f = new Functions();
             ILambdaContext ctx = Substitute.For<ILambdaContext>();
@@ -48,6 +109,36 @@ namespace Noti.Tests
             };
             var response = f.FunctionHandler(request, null);
 
+            Console.WriteLine(((PlainTextOutputSpeech) response.Response.OutputSpeech).Text);
+        }
+
+        
+        [Fact]
+        public void Check()
+        {
+            Functions f = new Functions();
+            ILambdaContext ctx = Substitute.For<ILambdaContext>();
+            var request = new SkillRequest
+            {
+                Request = new RequestBundle
+                {
+                    Type = "IntentRequest",
+                    Intent = new Intent
+                    {
+                        Name = "Check",
+                        Slots = new Dictionary<string, Slot> {
+                        }
+                    }
+                },
+                Session = new Session {
+                    User = new User {
+                        UserId = "jill"
+                    }
+                }
+            };
+            var response = f.FunctionHandler(request, null);
+
+            Console.WriteLine(((PlainTextOutputSpeech) response.Response.OutputSpeech).Text);
         }
     }
 }
