b0d378eda0a8353d58e10f322258817e017e44a6
Robert Stiff <uatecuk@gmail.com>
2017-05-11 23:37:14 +0100

    List friends.

---
+++ src/Noti/Intents/ListFriendsIntent.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Noti.Models;
+using ServiceStack.Redis;
+
+namespace Noti.Intents
+{
+    [Utterance("Who is in my phone book")]
+    [Utterance("Who can I send messages to")]
+    internal class ListFriendsIntent : IntentBase
+    {
+        IRedisClient _client;
+        private Context ctx;
+
+        public ListFriendsIntent(Context ctx, IRedisClient client)
+        {
+            _client = client;
+            this.ctx = ctx;
+        }
+        
+        public string Invoke(string recipient, string message)
+        {
+            var addressBook = getAddressBook(this.ctx.UserId);
+
+            if (addressBook.Keys.Any()) 
+            {
+                return string.Join(", ", addressBook.Keys);
+            }
+
+            return $"You don't have anyone in your phonebook yet. To send messages, get a friend code and then befriend them with that code.";
+        }
+        private Dictionary<string, string> getAddressBook(string userId)
+        {
+            _client.Db = RedisDBs.AddressBooks;
+            return _client.As<Dictionary<string, string>>().GetValue(userId) ?? new Dictionary<string, string>();
+        }
+    }
+}
